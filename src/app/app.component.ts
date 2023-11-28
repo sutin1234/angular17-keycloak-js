ffcccb110c2693fc8e814f14c2be642e874c703f
Sutin <poly.dev2018@gmail.com>
2023-11-28 19:54:57 +0700

    add keycloak 23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {afterNextRender, Component, effect, inject, Inject, OnInit, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import Keycloak, {KeycloakProfile, KeycloakTokenParsed} from "keycloak-js";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,43 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-keycloak-js';
+  authenticated = signal(false)
+  userProfile = signal<{name: string, preferred_username: string} | null>(null)
+  kc!: Keycloak
+  onReady = (auth: boolean) => auth
+
+  constructor() {
+    afterNextRender(async () => {
+      if (typeof window !== 'undefined') {
+         this.kc = new Keycloak({
+          url: 'http://localhost:8080',
+          realm: 'angular_dev',
+          clientId: 'e_commerce'
+        })
+
+        this.kc.init({ onLoad: 'login-required', checkLoginIframe: false}).then(auth => {
+          this.authenticated.set(auth)
+          this.userProfile.set(this.kc.idTokenParsed as any)
+        })
+
+      }
+
+    })
+    effect(() => {
+      console.log('authState changed ', this.authenticated(), this.userProfile())
+    });
+  }
+
+
+  ngOnInit(): void {
+    if(this.kc !== undefined){
+     console.log(this.kc)
+    }
+
+
+
+  }
+
 }
